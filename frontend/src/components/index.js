ecb2af9464ecf0962981f086717575d53270f8e1
kombe <kombe.kampanga@hotmail.com>
2024-03-07 15:31:33 +1300

    Revert "initial"
    This reverts commit 1c7aaff9969df325beca4ffbcddd49585eb9a8ea.

--- frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import HomeContent from "./home-content";
-import Footer from "./footer";
-import Loading from "./loading";
-import NavBar from "./nav-bar";
-
-export { HomeContent, Footer, Loading, NavBar };
